5c7cbbd55b2b900b5657648a03d23244bfb7fc05
wannabilmz <wannabilmuz@gmail.com>
2025-09-12 02:17:06 +0700

    Add files via upload

---
+++ Praktikum_01.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum_01;
+
+/**
+ *
+ * @author Nabil
+ */
+public class Praktikum_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Wan Nabil Muzakhy");
+        System.out.println("2407127390");
+    }
+}
