343b997f73038df1126391e522f35c6c6fd1195f
herva22 <herva.sulistia0856@student.unri.ac.id>
2025-10-21 19:38:26 +0700

    Add files via upload

---
+++ Praktikum_1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum_1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Praktikum_1 {
+
+    public static void main(String[] args) {
+        System.out.println("nama Herva Sulistia");
+        System.out.println("Nim 2407110856");
+    }
+}
